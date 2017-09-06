646f1a8175bbd49e8850f74f01522972af0bb4a8
Doam Dishon <noam.dishon@wework.com>
2017-09-06 16:32:53 +0300

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+genres = Genre.create([  {name: 'Vegetarian', icon:'H'},
+                                  {name: 'Pizza', icon:'I'},
+                                  {name: 'Hamburger', icon:'A'},
+                                  {name: 'Coffee', icon:'b'}
+                                  ])
+
+restaurants = Restaurant.create( [{name:'Vegi-Vegi', ten_bis:true, genre: genres[0]},
+                                           {name:'Big Mamma', ten_bis:false, genre: genres[1], geo:'32.0674777,34.78097768'},
+                                           {name:'Moses', ten_bis:true, genre: genres[3]},
+                                           {name:'Aroma', ten_bis:true, genre: genres[3]}
+                                          ])
+Review.create( [ {name: 'Ben', text: 'Good Place', restaurant: restaurants[0], rating: 1},
+                          {name: 'Dan', text: 'OK Place', restaurant: restaurants[0], rating: 3},
+                          {name: 'Moshe', text: 'Bad Place', restaurant: restaurants[1], rating: 1}
+
+               ])
